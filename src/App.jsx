dac794e4fcb3adcd5190b0e95625d86f67c29ccc
zaalbear <zaalbear@ZaalbearNote.zaalbear>
2024-05-17 19:26:37 -0300

    feat: created application

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { Slider } from "./components/Slider"
+import "./styles/reset.css"
+import "./styles/globalStyles.css"
+
+function App() {
+
+  return (
+    <>
+      <Slider></Slider>
+    </>
+  )
+}
+
+export default App
