a180f3475b4f6012d6d91600ea2ef9747855fd25
João Barcellos <dev.zaalbear@gmail.com>
2024-05-18 10:28:08 -0300

    added local free copyright images and hyperlink to each pexels website endpoint

--- src/components/Slider/index.jsx
+++ src/components/Slider/index.jsx
@@ -1,43 +1,78 @@
-import { useEffect, useState } from "react"
-import { imageArray } from "../../data/database.js"
-import styles from "./styles.module.css"
+import { useEffect, useState } from "react";
+import { imageArray } from "../../data/database.js";
+import styles from "./styles.module.css";
 
-import CircleIcon from '@mui/icons-material/Circle';
-import ArrowDropDownCircleIcon from '@mui/icons-material/ArrowDropDownCircle';
+import CircleIcon from "@mui/icons-material/Circle";
+import ArrowDropDownCircleIcon from "@mui/icons-material/ArrowDropDownCircle";
 
 export const Slider = () => {
-    const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
-    useEffect(() => {
-        const interval = setInterval(() => {
-            setCount(prevCount => (prevCount + 1) % imageArray.length);
-        }, 8000);
+  useEffect(() => {
+    const interval = setInterval(() => {
+      setCount((prevCount) => (prevCount + 1) % imageArray.length);
+    }, 8000);
 
-        return () => clearInterval(interval);
-    }, []);
+    return () => clearInterval(interval);
+  }, []);
 
-    return (
-        <ul className={styles.image_list}>
-            {imageArray.map((image, index) => {
-                return (
-                    <li className={styles.image_container} key={index}>
-                        {index === count ? <img className={styles.image} src={image.URL} alt={image.ALT} /> : null}
-                    </li>
-                )
-            })}
+  return (
+    <ul className={styles.image_list}>
+      <a
+        href={imageArray[count].LINK}
+        target="_blank"
+        className={styles.image_link}
+      >
+        {imageArray.map((image, index) => {
+          return (
+            <li className={styles.image_container} key={index}>
+              {index === count ? (
+                <img className={styles.image} src={image.URL} alt={image.ALT} />
+              ) : null}
+            </li>
+          );
+        })}
+      </a>
 
-            <ul className={styles.dot_list}>
-                {imageArray.map((image, index) => {
-                    return (
-                        <span className={styles.dot_item} key={index}>
-                            <CircleIcon className={index === count ? styles.active : styles.inactive} fontSize="medium" onClick={() => { setCount(index) }} />
-                        </span>
-                    )
-                })}
-
-            </ul>
-            <button className={`${styles.button} ${styles.left}`} onClick={() => { count - 1 >= 0 ? setCount(count - 1) : setCount(imageArray.length - 1) }}><ArrowDropDownCircleIcon className={styles.button_icon} fontSize="large" /></button>
-            <button className={`${styles.button} ${styles.rigth}`} onClick={() => { count + 1 < imageArray.length ? setCount(count + 1) : setCount(0) }}><ArrowDropDownCircleIcon className={styles.button_icon} fontSize="large" /></button>
-        </ul>
-    )
-}
+      <ul className={styles.dot_list}>
+        {imageArray.map((image, index) => {
+          return (
+            <span className={styles.dot_item} key={index}>
+              <CircleIcon
+                className={index === count ? styles.active : styles.inactive}
+                fontSize="medium"
+                onClick={() => {
+                  setCount(index);
+                }}
+              />
+            </span>
+          );
+        })}
+      </ul>
+      <button
+        className={`${styles.button} ${styles.left}`}
+        onClick={() => {
+          count - 1 >= 0
+            ? setCount(count - 1)
+            : setCount(imageArray.length - 1);
+        }}
+      >
+        <ArrowDropDownCircleIcon
+          className={styles.button_icon}
+          fontSize="large"
+        />
+      </button>
+      <button
+        className={`${styles.button} ${styles.rigth}`}
+        onClick={() => {
+          count + 1 < imageArray.length ? setCount(count + 1) : setCount(0);
+        }}
+      >
+        <ArrowDropDownCircleIcon
+          className={styles.button_icon}
+          fontSize="large"
+        />
+      </button>
+    </ul>
+  );
+};
--- src/data/database.js
+++ src/data/database.js
@@ -1,22 +1,27 @@
 export const imageArray = [
     {
-        URL: "https://store-images.s-microsoft.com/image/apps.756.13879366447690213.ff0dcdf1-af01-4af7-b105-db57cef9cb67.f2db4bb3-e1b0-4b73-a1b9-95d7e73c78ea",
-        ALT: "synthwave sundown",
-        LINK: "https://store-images.s-microsoft.com/image/apps.756.13879366447690213.ff0dcdf1-af01-4af7-b105-db57cef9cb67.f2db4bb3-e1b0-4b73-a1b9-95d7e73c78ea",
+        URL: "/src/assets/pexels-moose-photos.jpg",
+        ALT: "white headphone",
+        LINK: "https://www.pexels.com/pt-br/foto/fone-de-ouvido-branco-1037992/",
     },
     {
-        URL: "https://steamuserimages-a.akamaihd.net/ugc/940586530515504757/CDDE77CB810474E1C07B945E40AE4713141AFD76/?imw=5000&imh=5000&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false",
-        ALT: "unrealist ink monster art",
-        LINK: "https://steamuserimages-a.akamaihd.net/ugc/940586530515504757/CDDE77CB810474E1C07B945E40AE4713141AFD76/?imw=5000&imh=5000&ima=fit&impolicy=Letterbox&imcolor=%23000000&letterbox=false",
+        URL: "/src/assets/pexels-pixabay.jpg",
+        ALT: "yellow tree",
+        LINK: "https://www.pexels.com/pt-br/foto/arvore-de-folhas-marrons-em-campo-aberto-sob-nuvens-brancas-e-ceu-azul-35857/",
     },
     {
-        URL: "https://www.lifewire.com/thmb/CLI14Ku63Uujk5VlROmhN67G-3I=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/wallhaven-5919d60a5f9b58f4c04e8b26.png",
-        ALT: "girl with a umbrela",
-        LINK: "https://www.lifewire.com/thmb/CLI14Ku63Uujk5VlROmhN67G-3I=/1500x0/filters:no_upscale():max_bytes(150000):strip_icc()/wallhaven-5919d60a5f9b58f4c04e8b26.png",
+        URL: "/src/assets/pexels-steve.jpg",
+        ALT: "Abstract paint",
+        LINK: "https://www.pexels.com/pt-br/foto/pintura-abstrata-em-azul-branco-e-laranja-1292241/",
     },
     {
-        URL: "https://macmagazine.com.br/wp-content/uploads/2022/09/12-wallpaper-iphone-14-idownloadblog.png",
-        ALT: "two smartphones",
-        LINK: "https://macmagazine.com.br/wp-content/uploads/2022/09/12-wallpaper-iphone-14-idownloadblog.png",
+        URL: "/src/assets/pexels-timothy-paule-ii.jpg",
+        ALT: "Neon Lights",
+        LINK: "https://www.pexels.com/pt-br/foto/seja-luz-neon-brilhante-2002719/",
+    },
+    {
+        URL: "/src/assets/pexels-magda-ehlers.jpg",
+        ALT: "raibow wallpaper",
+        LINK: "https://www.pexels.com/pt-br/foto/texto-colorido-1279813/",
     },
 ]
