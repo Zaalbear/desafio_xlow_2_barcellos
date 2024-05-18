f71a7d319e432f377deee6464aea469531e0187b
João Barcellos <dev.zaalbear@gmail.com>
2024-05-18 10:37:06 -0300

    fix bug with local image path

--- src/data/database.js
+++ src/data/database.js
@@ -1,26 +1,32 @@
+import whiteheadphones from "../assets/pexels-moose-photos.jpg"
+import yellowtree  from "../assets/pexels-pixabay.jpg"
+import abstractpaint from "../assets/pexels-steve.jpg"
+import neonlights from "../assets/pexels-timothy-paule-ii.jpg"
+import rainbow from "../assets/pexels-magda-ehlers.jpg"
+
 export const imageArray = [
     {
-        URL: "../../src/assets/pexels-moose-photos.jpg",
+        URL: whiteheadphones,
         ALT: "white headphone",
         LINK: "https://www.pexels.com/pt-br/foto/fone-de-ouvido-branco-1037992/",
     },
     {
-        URL: "../../src/assets/pexels-pixabay.jpg",
+        URL: yellowtree,
         ALT: "yellow tree",
         LINK: "https://www.pexels.com/pt-br/foto/arvore-de-folhas-marrons-em-campo-aberto-sob-nuvens-brancas-e-ceu-azul-35857/",
     },
     {
-        URL: "../../src/assets/pexels-steve.jpg",
+        URL: abstractpaint,
         ALT: "Abstract paint",
         LINK: "https://www.pexels.com/pt-br/foto/pintura-abstrata-em-azul-branco-e-laranja-1292241/",
     },
     {
-        URL: "../../src/assets/pexels-timothy-paule-ii.jpg",
+        URL: neonlights,
         ALT: "Neon Lights",
         LINK: "https://www.pexels.com/pt-br/foto/seja-luz-neon-brilhante-2002719/",
     },
     {
-        URL: "../../src/assets/pexels-magda-ehlers.jpg",
+        URL: rainbow,
         ALT: "raibow wallpaper",
         LINK: "https://www.pexels.com/pt-br/foto/texto-colorido-1279813/",
     },
