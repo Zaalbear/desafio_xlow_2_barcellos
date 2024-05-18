101ba38acbd1887bc00c645f6fa9ccd5286d4b06
João Barcellos <dev.zaalbear@gmail.com>
2024-05-18 10:32:26 -0300

    fix bug with local image path

--- src/data/database.js
+++ src/data/database.js
@@ -1,26 +1,26 @@
 export const imageArray = [
     {
-        URL: "/src/assets/pexels-moose-photos.jpg",
+        URL: "../../src/assets/pexels-moose-photos.jpg",
         ALT: "white headphone",
         LINK: "https://www.pexels.com/pt-br/foto/fone-de-ouvido-branco-1037992/",
     },
     {
-        URL: "/src/assets/pexels-pixabay.jpg",
+        URL: "../../src/assets/pexels-pixabay.jpg",
         ALT: "yellow tree",
         LINK: "https://www.pexels.com/pt-br/foto/arvore-de-folhas-marrons-em-campo-aberto-sob-nuvens-brancas-e-ceu-azul-35857/",
     },
     {
-        URL: "/src/assets/pexels-steve.jpg",
+        URL: "../../src/assets/pexels-steve.jpg",
         ALT: "Abstract paint",
         LINK: "https://www.pexels.com/pt-br/foto/pintura-abstrata-em-azul-branco-e-laranja-1292241/",
     },
     {
-        URL: "/src/assets/pexels-timothy-paule-ii.jpg",
+        URL: "../../src/assets/pexels-timothy-paule-ii.jpg",
         ALT: "Neon Lights",
         LINK: "https://www.pexels.com/pt-br/foto/seja-luz-neon-brilhante-2002719/",
     },
     {
-        URL: "/src/assets/pexels-magda-ehlers.jpg",
+        URL: "../../src/assets/pexels-magda-ehlers.jpg",
         ALT: "raibow wallpaper",
         LINK: "https://www.pexels.com/pt-br/foto/texto-colorido-1279813/",
     },
